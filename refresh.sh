b903a3b1eaceebdbc99c8fa7275118f998061c78
Orleika <orleika@users.noreply.github.com>
2016-04-14 22:29:40 +0900

    modify refer path

--- refresh.sh
+++ refresh.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -eu
 
+dotfiles_dir=$(cd $(dirname $0) && pwd)
+
 plugins=(.zplug .enhancd)
 for dir in ${plugins[@]}; do
     if [ -d ~/${dir} ]; then
@@ -8,13 +10,16 @@ for dir in ${plugins[@]}; do
     fi
 done
 
-dotfiles=(.zshrc .zprofile .exports .aliases .functions .gitconfig .editorconfig)
-for file in ${dotfiles[@]}; do
-    if [ -f ~/${file} ]; then
-        rm ~/${file}
+dotfiles=`find $dotfiles_dir -maxdepth 1 -type f -name ".*"`
+for file in $dotfiles; do
+    if [ -f ~/${file##*/} ]; then
+        rm ~/${file##*/}
     fi
 done
 
-if [ -f ~/bin/lessfilter ]; then
-    rm ~/bin/lessfilter
-fi
+binaries=`find ${dotfiles_dir}/bin -maxdepth 1 -type f`
+for file in $dotfiles; do
+    if [ -f ~/bin/${file##*/} ]; then
+        rm ~/bin/${file##*/}
+    fi
+done
--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -eu
 
+dotfiles_dir=$(cd $(dirname $0) && pwd)
+
 # setup zsh
 chsh -s `which zsh`
 
@@ -8,20 +10,23 @@ chsh -s `which zsh`
 curl -fLo ~/.zplug/zplug --create-dirs https://git.io/zplug
 
 # symlinks
-dotfiles=(.zshrc .zprofile .exports .aliases .functions .gitconfig .editorconfig)
-for file in ${dotfiles[@]}; do
-    if [ -f ~/${file} ]; then
-        rm ~/${file}
+dotfiles=`find $dotfiles_dir -maxdepth 1 -type f -name ".*"`
+for file in $dotfiles; do
+    if [ -f ~/${file##*/} ]; then
+        rm ~/${file##*/}
     fi
-    ln -s ~/dotfiles/${file} ~/${file}
+    ln -s ${file} ~/${file##*/}
 done
 if [ ! -d ~/bin ]; then
     mkdir ~/bin
 fi
-if [ -f ~/bin/lessfilter ]; then
-    rm ~/bin/lessfilter
-fi
-ln -s ~/dotfiles/lessfilter ~/bin/lessfilter
+binaries=`find ${dotfiles_dir}/bin -maxdepth 1 -type f`
+for file in $dotfiles; do
+    if [ -f ~/bin/${file##*/} ]; then
+        rm ~/bin/${file##*/}
+    fi
+    ln -s ${file} ~/bin/${file##*/}
+done
 
 # reflesh shell
 exec -l $SHELL
