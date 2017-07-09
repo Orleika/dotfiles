55cf7439c7986c1b5ac11404969cba391d2bdbde
orleika <admin@orleika.io>
2017-07-09 17:30:21 +0000

    Adjust to POSIX sh

--- refresh.sh
+++ refresh.sh
@@ -1,25 +1,25 @@
-#!/bin/bash
+#!/bin/sh
 set -eu
 
-dotfiles_dir=$(cd $(dirname $0) && pwd)
+dotfiles_dir=$(cd "$(dirname "$0")" && pwd)
 
-plugins=(.zplug .enhancd)
-for dir in ${plugins[@]}; do
-    if [ -d ~/${dir} ]; then
-        rm -Rf ~/${dir}
+plugins=".zplug .enhancd"
+for dir in ${plugins}; do
+    if [ -d ~/"${dir}" ]; then
+        rm -Rf ~/"${dir}"
     fi
 done
 
-dotfiles=`find $dotfiles_dir -maxdepth 1 -type f -name ".*"`
+dotfiles=$(find "$dotfiles_dir" -maxdepth 1 -type f -name ".*")
 for file in $dotfiles; do
-    if [ -f ~/${file##*/} ]; then
-        rm ~/${file##*/}
+    if [ -f ~/"${file##*/}" ]; then
+        rm ~/"${file##*/}"
     fi
 done
 
-binaries=`find ${dotfiles_dir}/bin -maxdepth 1 -type f`
+binaries=$(find "${dotfiles_dir}"/bin -maxdepth 1 -type f)
 for file in $binaries; do
-    if [ -f ~/bin/${file##*/} ]; then
-        rm ~/bin/${file##*/}
+    if [ -f ~/bin/"${file##*/}" ]; then
+        rm ~/bin/"${file##*/}"
     fi
 done
--- setup.sh
+++ setup.sh
@@ -1,35 +1,36 @@
-#!/bin/bash
+#!/bin/sh
 set -eu
 
-dotfiles_dir=$(cd $(dirname $0) && pwd)
+dotfiles_dir=$(cd "$(dirname "$0")" && pwd)
 
 # symlinks
-dotfiles=`find $dotfiles_dir -maxdepth 1 -type f -name ".*"`
+dotfiles=$(find "$dotfiles_dir" -maxdepth 1 -type f -name ".*")
 for file in $dotfiles; do
-    if [ -f ~/${file##*/} ]; then
-        rm ~/${file##*/}
+    if [ -f ~/"${file##*/}" ]; then
+        rm ~/"${file##*/}"
     fi
-    ln -s ${file} ~/${file##*/}
+    ln -s "${file}" ~/"${file##*/}"
 done
 if [ ! -d ~/bin ]; then
     mkdir ~/bin
 fi
-binaries=`find ${dotfiles_dir}/bin -maxdepth 1 -type f`
+binaries=$(find "${dotfiles_dir}"/bin -maxdepth 1 -type f)
 for file in $binaries; do
-    if [ -f ~/bin/${file##*/} ]; then
-        rm ~/bin/${file##*/}
+    if [ -f ~/bin/"${file##*/}" ]; then
+        rm ~/bin/"${file##*/}"
     fi
-    ln -s ${file} ~/bin/${file##*/}
+    ln -s "${file}" ~/bin/"${file##*/}"
 done
 if [ ! -d ~/.ssh ]; then
     mkdir ~/.ssh
 fi
-sshfiles=`find ${dotfiles_dir}/.ssh -maxdepth 1 -type f`
+sshfiles=$(find "${dotfiles_dir}"/.ssh -maxdepth 1 -type f)
 for file in $sshfiles; do
-    cat ${file} >> ~/.ssh/${file##*/}
+    install -m 0600 "${file}" ~/.ssh/"${file##*/}"
 done
 
 # install zplug
-source $dotfiles_dir/.exports
-git clone https://github.com/zplug/zplug $ZPLUG_HOME
+# shellcheck source=/dev/null
+. "$dotfiles_dir"/.exports
+git clone https://github.com/zplug/zplug "$ZPLUG_HOME"
 
